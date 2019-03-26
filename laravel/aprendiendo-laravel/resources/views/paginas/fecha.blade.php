d37a562815cb3b28c84248e53ff28c8eca73a015
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-03-26 10:38:15 -0600

    Initial commit

---
+++ laravel/aprendiendo-laravel/resources/views/paginas/fecha.blade.php
@@ -0,0 +1,11 @@
+<?php
+
+    /**
+     * Vista que muestra la fecha actual del sistema
+    */
+
+    echo "<h1> $titulo </h1>";
+
+    echo date('d-m-Y');
+
+ ?>
---
+++ laravel/aprendiendo-laravel/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/fecha', function () {
+    $titulo = "Fecha del día de hoy";
+    return view('paginas.fecha', array(
+        'titulo' => $titulo
+    ));
+});
+
+/**
+ * Esto es cuando el parametro es de manera
+ * obligatoria
+ */
+// Route::get('/pelicula/{titulo}', function ($titulo) {
+//     return view('paginas.pelicula', array(
+//         'titulo' => $titulo
+//     ));
+// });
+
+/**
+ * Esto es cuando el parametro es de manera
+ * opcional cambia con un {titulo"?"} y dando 
+ * valor por defecto a la variable
+ */
+Route::get('/pelicula/{titulo?}', function ($titulo = 'No pusiste pelicula') {
+    return view('paginas.pelicula', array(
+        'titulo' => $titulo
+    ));
+});
