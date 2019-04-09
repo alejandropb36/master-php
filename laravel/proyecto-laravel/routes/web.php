a204ec0b8a045af1b93e24d49cf61343ab6cab21
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-04-09 11:52:07 -0500

    Base de datos y entidades del proyecto de Laravel
     Terminado !!

--- laravel/proyecto-laravel/routes/web.php
+++ laravel/proyecto-laravel/routes/web.php
@@ -10,7 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Image;
+
 
 Route::get('/', function () {
+
+    $images = Image::all();
+    foreach($images as $image){
+        echo $image->image_path . "<br/>";
+        echo $image->description . "<br/>";
+        echo $image->user->name . " " . $image->user->surname . "<br/>";
+        echo '<strong>LIKES: ' . count($image->likes) . '</strong>';
+        if(count($image->comments) >= 1)
+        {
+            echo "<h4> Comentarios </h4>";
+            foreach($image->comments as $comment){
+                echo $comment->user->name . " " . $comment->user->surname . ": ";
+                echo $comment->content . "<br>";
+            }  
+        }
+        echo "<hr/>";
+    }
+    die();
     return view('welcome');
 });
