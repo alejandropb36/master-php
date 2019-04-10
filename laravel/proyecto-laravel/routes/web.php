93cc3bddb9a3c8d797511397c35b3b717ed58ed0
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-04-10 13:38:36 -0500

    Login y registro de usuarios en Laravel: login (make:auth)

--- laravel/proyecto-laravel/routes/web.php
+++ laravel/proyecto-laravel/routes/web.php
@@ -10,11 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-use App\Image;
+//use App\Image;
 
 
 Route::get('/', function () {
 
+    /*
     $images = Image::all();
     foreach($images as $image){
         echo $image->image_path . "<br/>";
@@ -32,5 +33,10 @@ Route::get('/', function () {
         echo "<hr/>";
     }
     die();
+    */
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
