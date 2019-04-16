3f4d2ccd559886389e69071fa3d2d03bbd7a809f
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-04-15 20:13:23 -0500

    Pagina de configuracion de usuario: Actualizacion de usuario

--- laravel/proyecto-laravel/app/Http/Controllers/UserController.php
+++ laravel/proyecto-laravel/app/Http/Controllers/UserController.php
@@ -10,4 +10,36 @@ class UserController extends Controller
     public function config(){
         return view('user.config');
     }
+
+    public function update(Request $request){
+        // Conseguri el usuario identificado
+        $user = \Auth::user();
+        $id = $user->id;
+
+        // Validacion de los campos del usuario
+        $validate = $this->validate($request,[
+            'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
+            'nick' => ['required', 'string', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],           
+        ]);
+
+        // Recoger los datos del formulario
+        $name = $request->input('name');
+        $surname = $request->input('surname');
+        $nick = $request->input('nick');
+        $email = $request->input('email');
+
+        // Asigar los nuevos valores al objeto del usuario
+        $user->name = $name;
+        $user->surname = $surname;
+        $user->nick = $nick;
+        $user->email= $email;
+
+        // Ejecutar consulta y cambios en la base de datos
+        $user->update();
+
+        return redirect()->route('config')
+                                ->with(['message' => 'El usuario se actualizo correctamente']);
+    }
 }
--- laravel/proyecto-laravel/resources/views/user/config.blade.php
+++ laravel/proyecto-laravel/resources/views/user/config.blade.php
@@ -7,18 +7,25 @@
     <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-8">
+                    {{-- Esto es para mostrar el mensaje de actualizacion --}}
+                    @if (session('message'))
+                        <div class="alert alert-success">
+                            {{session('message')}}
+                        </div>
+                    @endif
+
                     <div class="card">
                         <div class="card-header">Configuración de mi cuenta</div>
         
                         <div class="card-body">
-                            <form method="POST" action="">
+                            <form method="POST" action=" {{ route('user.update')}} ">
                                 @csrf
         
                                 <div class="form-group row">
                                     <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
         
                                     <div class="col-md-6">
-                                        <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                                        <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ Auth::user()->name }}" required autofocus>
         
                                         @if ($errors->has('name'))
                                             <span class="invalid-feedback" role="alert">
@@ -32,7 +39,7 @@
                                     <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
         
                                     <div class="col-md-6">
-                                        <input id="surname" type="text" class="form-control{{ $errors->has('surname') ? ' is-invalid' : '' }}" name="surname" value="{{ old('surname') }}" required autofocus>
+                                        <input id="surname" type="text" class="form-control{{ $errors->has('surname') ? ' is-invalid' : '' }}" name="surname" value="{{ Auth::user()->surname }}" required autofocus>
         
                                         @if ($errors->has('surname'))
                                             <span class="invalid-feedback" role="alert">
@@ -46,7 +53,7 @@
                                     <label for="nick" class="col-md-4 col-form-label text-md-right">{{ __('Nick') }}</label>
         
                                     <div class="col-md-6">
-                                        <input id="nick" type="text" class="form-control{{ $errors->has('nick') ? ' is-invalid' : '' }}" name="nick" value="{{ old('nick') }}" required autofocus>
+                                        <input id="nick" type="text" class="form-control{{ $errors->has('nick') ? ' is-invalid' : '' }}" name="nick" value="{{ Auth::user()->nick }}" required autofocus>
         
                                         @if ($errors->has('nick'))
                                             <span class="invalid-feedback" role="alert">
@@ -60,7 +67,7 @@
                                     <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
         
                                     <div class="col-md-6">
-                                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ Auth::user()->email }}" required>
         
                                         @if ($errors->has('email'))
                                             <span class="invalid-feedback" role="alert">
