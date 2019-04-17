1b338e147936330d7da4baaab03cb86602fc931f
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-04-16 20:41:07 -0500

    Pagina de configuracion del usuario: subir imagen

--- laravel/proyecto-laravel/database.sql
+++ laravel/proyecto-laravel/database.sql
@@ -21,9 +21,6 @@ CREATE TABLE IF NOT EXISTS users(
     CONSTRAINT pk_users PRIMARY KEY (id)
 )ENGINE=InnoDb;
 
-INSERT INTO users VALUES(null, 'user', 'Alejandro', 'Ponce', 'alejandro.ponce', 'alejandro.ponce@email.com', 'pass', null, CURTIME(), CURTIME(), null);
-INSERT INTO users VALUES(null, 'user', 'Karla', 'Jazmin', 'karla.jazmin', 'karla.jazmin@email.com', 'pass', null, CURTIME(), CURTIME(), null);
-INSERT INTO users VALUES(null, 'user', 'Pedro', 'Aguayo', 'pedro.aguayo', 'pedro.aguayo@email.com', 'pass', null, CURTIME(), CURTIME(), null);
 
 -- Tabla de Imagenes
 CREATE TABLE IF NOT EXISTS images(
@@ -36,11 +33,6 @@ CREATE TABLE IF NOT EXISTS images(
     CONSTRAINT pk_images PRIMARY KEY (id),
     CONSTRAINT fk_images_users FOREIGN KEY (user_id) REFERENCES users(id)
 )ENGINE=InnoDb;
-
-INSERT INTO images VALUES(null, 1, 'test.jpg', 'image 1', CURTIME(), CURTIME());
-INSERT INTO images VALUES(null, 1, 'playa.jpg', 'image 1', CURTIME(), CURTIME());
-INSERT INTO images VALUES(null, 1, 'arena.jpg', 'image 1', CURTIME(), CURTIME());
-INSERT INTO images VALUES(null, 3, 'familia.jpg', 'image 3', CURTIME(), CURTIME());
 
 
 -- Tabla de Comentarios
@@ -56,9 +48,6 @@ CREATE TABLE IF NOT EXISTS comments(
     CONSTRAINT fk_comments_images FOREIGN KEY (image_id) REFERENCES images(id)  
 )ENGINE=InnoDb;
 
-INSERT INTO comments VALUES(null, 1, 4, 'Buena foto de familia!!', CURTIME(), CURTIME());
-INSERT INTO comments VALUES(null, 2, 1, 'Buena foto de playa!!', CURTIME(), CURTIME());
-INSERT INTO comments VALUES(null, 2, 4, 'que bueno!!', CURTIME(), CURTIME());
 
 -- Tabla de Likes
 CREATE TABLE IF NOT EXISTS likes(
@@ -71,6 +60,19 @@ CREATE TABLE IF NOT EXISTS likes(
     CONSTRAINT fk_likes_users FOREIGN KEY (user_id) REFERENCES users(id),
     CONSTRAINT fk_likes_images FOREIGN KEY (image_id) REFERENCES images(id)  
 )ENGINE=InnoDb;
+
+INSERT INTO users VALUES(null, 'user', 'Alejandro', 'Ponce', 'alejandro.ponce', 'alejandro.ponce@email.com', 'pass', null, CURTIME(), CURTIME(), null);
+INSERT INTO users VALUES(null, 'user', 'Karla', 'Jazmin', 'karla.jazmin', 'karla.jazmin@email.com', 'pass', null, CURTIME(), CURTIME(), null);
+INSERT INTO users VALUES(null, 'user', 'Pedro', 'Aguayo', 'pedro.aguayo', 'pedro.aguayo@email.com', 'pass', null, CURTIME(), CURTIME(), null);
+
+INSERT INTO images VALUES(null, 1, 'test.jpg', 'image 1', CURTIME(), CURTIME());
+INSERT INTO images VALUES(null, 1, 'playa.jpg', 'image 1', CURTIME(), CURTIME());
+INSERT INTO images VALUES(null, 1, 'arena.jpg', 'image 1', CURTIME(), CURTIME());
+INSERT INTO images VALUES(null, 3, 'familia.jpg', 'image 3', CURTIME(), CURTIME());
+
+INSERT INTO comments VALUES(null, 1, 4, 'Buena foto de familia!!', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(null, 2, 1, 'Buena foto de playa!!', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(null, 2, 4, 'que bueno!!', CURTIME(), CURTIME());
 
 INSERT INTO likes VALUES(null, 1, 4, CURTIME(), CURTIME());
 INSERT INTO likes VALUES(null, 2, 4, CURTIME(), CURTIME());
--- laravel/proyecto-laravel/resources/views/user/config.blade.php
+++ laravel/proyecto-laravel/resources/views/user/config.blade.php
@@ -18,7 +18,7 @@
                         <div class="card-header">Configuración de mi cuenta</div>
         
                         <div class="card-body">
-                            <form method="POST" action=" {{ route('user.update')}} ">
+                            <form method="POST" action=" {{ route('user.update')}} " enctype="multipart/form-data" aria-label="Configuracion de mi usuario">
                                 @csrf
         
                                 <div class="form-group row">
@@ -72,6 +72,20 @@
                                         @if ($errors->has('email'))
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('Avatar') }}</label>
+        
+                                    <div class="col-md-6">
+                                        <input id="image" type="file" class="form-control{{ $errors->has('image') ? ' is-invalid' : '' }}" name="image">
+        
+                                        @if ($errors->has('image'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('image') }}</strong>
                                             </span>
                                         @endif
                                     </div>
