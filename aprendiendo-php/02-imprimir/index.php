a2ff26f4a893aec253229a0639550b078c902eeb
Alejandro Ponce <alexponce36@hotmail.com>
2020-01-11 13:17:54 -0600

    Primeros pasos php: impresion por pantalla y comentarios

---
+++ aprendiendo-php/02-imprimir/index.php
@@ -0,0 +1,20 @@
+<!Doctype HTML>
+<html>
+    <head>
+        <title>Imprimir por pantalla- Master en PHP</title>
+    </head>
+    <body>
+        <h1>Master en PHP</h1>
+        // Comenatrio
+        <?php
+            echo '<h3>Listado de Videojuegos </h3>';
+            echo "<ul>"
+                . "<li>GTA V</li>"
+                . "<li>Fifa</li>"
+                . "<li>Mario bros</li>"
+                . "<li> LOL </li>"
+                . "</ul>";
+        ?>
+
+    </body>
+</html>
