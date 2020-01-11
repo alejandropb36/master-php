8268e27bafd713cf521d6eaab6c21cf86286f108
Alejandro Ponce <alexponce36@hotmail.com>
2020-01-11 13:32:32 -0600

    Variables superglobales: servidor

---
+++ aprendiendo-php/07-superglobales/index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Variables super globaels
+
+// de servidor
+
+echo $_SERVER['SERVER_ADDR'];
+echo $_SERVER['SERVER_NAME'];
+echo $_SERVER['SERVER_SOFTWARE'];
+echo $_SERVER['HTTP_USER_AGENT'];
+echo $_SERVER['REMOTE_ADDR'];
