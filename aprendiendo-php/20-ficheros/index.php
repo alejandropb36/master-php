69803f37f191f7f5856dc7ab15a45ff8b15cea73
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2020-02-13 18:48:28 -0600

    sistema de archivos: manipulacion de archivos

--- aprendiendo-php/20-ficheros/index.php
+++ aprendiendo-php/20-ficheros/index.php
@@ -1,12 +1,21 @@
 <?php
 
-$archivo = fopen("fichero_texto.txt", "a+");
-while (!feof($archivo)) {
-    $contenido = fgets($archivo);
-    echo $contenido;
-    echo "<br>";
-}
+// $archivo = fopen("fichero_texto.txt", "a+");
+// while (!feof($archivo)) {
+//     $contenido = fgets($archivo);
+//     echo $contenido;
+//     echo "<br>";
+// }
 
-fwrite($archivo, "Soy un texto escrito desde php");
+// fwrite($archivo, "Soy un texto escrito desde php");
 
-fclose($archivo);
+// fclose($archivo);
+
+// copiar
+// copy("fichero_texto.txt", "fichero_copiado.txt") or die("Error al copiar");
+
+// Renombrar
+//rename ("fichero_copiado.txt", "fichero_renombrado.txt");
+
+// Eliminar
+//unlink("fichero_renombrado.txt");
