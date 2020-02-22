597d2dc63e1cb35e286ef2d5557318cc722038a8
Alejandro Ponce <alexponce36@hotmail.com>
2020-02-22 13:14:35 -0600

    Tablas, tipos y restricciones: Modificar columnas

--- aprendiendo-sql/02-modificar-tablas/modificar.sql
+++ aprendiendo-sql/02-modificar-tablas/modificar.sql
@@ -6,3 +6,12 @@ alter table usuarios_renom change apellidos apellido varchar(100) null;
 
 -- Modificar columna sin cambiar nombre
 alter table usuarios_renom modify apellido char(50) not null;
+
+-- Añadir columna
+alter table usuarios_renom add website varchar(100) null;
+
+-- Añadir restriccion a columna
+alter table usuarios_renom add constraint uq_email unique(email);
+
+-- Borrar una columna
+alter table usuarios_renom drop website;
