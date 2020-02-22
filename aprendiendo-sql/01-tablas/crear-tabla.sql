05f2880812b0de7c2674f2571e946675ea1f42ea
Alejandro Ponce <alexponce36@hotmail.com>
2020-02-22 12:14:12 -0600

    Tablas, tipos y restricciones: Restricciones basicas

--- aprendiendo-sql/01-tablas/crear-tabla.sql
+++ aprendiendo-sql/01-tablas/crear-tabla.sql
@@ -3,9 +3,10 @@ Crear un tabla
 */
 
 create table usuarios (
-    id          int(11),
-    nombre      varchar(255),
-    apellidos   varchar(255),
-    email       varchar(100),
-    password    varchar(255)
+    id          int(11) auto_increment not null,
+    nombre      varchar(255) not null,
+    apellidos   varchar(255) default 'hola que tal',
+    email       varchar(100) not null,
+    password    varchar(255),
+    constraint pk_usuarios primary key (id)
 );
