9fbe7e185b3cff415347569a92b7a732262357b6
Alejandro Ponce <alexponce36@hotmail.com>
2020-02-22 11:48:32 -0600

    Tablas, tipos y restricciones: Crear tablas

---
+++ aprendiendo-sql/01-tablas/crear-tabla.sql
@@ -0,0 +1,11 @@
+/*
+Crear un tabla
+*/
+
+create table usuarios (
+    id          int(11),
+    nombre      varchar(255),
+    apellidos   varchar(255),
+    email       varchar(100),
+    password    varchar(255)
+);
