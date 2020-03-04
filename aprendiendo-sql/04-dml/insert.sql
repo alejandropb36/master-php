d305e58ba4207b2d3b2f31149b20bae21e696907
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2020-03-03 21:53:30 -0600

    DML: insertar

---
+++ aprendiendo-sql/04-dml/insert.sql
@@ -0,0 +1,4 @@
+-- Insertar nuevos registros
+insert into usuarios values (null, 'Alejandro', 'Ponce', 'alexponce36@hotmail.com', 'password', '2020-03-03');
+insert into usuarios values (null, 'Antonio', 'Martinez', 'antonio@mail.com', 'password', '2020-03-03');
+insert into usuarios values (null, 'Paco', 'Lopez', 'paco@mail.com', 'password', '2020-03-03');
