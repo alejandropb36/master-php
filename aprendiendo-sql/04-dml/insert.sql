55cd9317d97ee0e03dc18eb4f8529d7a225d31d7
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2020-03-06 22:21:59 -0600

    DML: insert especifico

--- aprendiendo-sql/04-dml/insert.sql
+++ aprendiendo-sql/04-dml/insert.sql
@@ -2,3 +2,6 @@
 insert into usuarios values (null, 'Alejandro', 'Ponce', 'alexponce36@hotmail.com', 'password', '2020-03-03');
 insert into usuarios values (null, 'Antonio', 'Martinez', 'antonio@mail.com', 'password', '2020-03-03');
 insert into usuarios values (null, 'Paco', 'Lopez', 'paco@mail.com', 'password', '2020-03-03');
+
+-- Insertar filas solo con ciertas columnas
+insert into usuarios (email, password) values ('admin@admin.com', 'admin');
