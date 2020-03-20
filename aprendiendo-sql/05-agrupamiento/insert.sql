f6338db8b2f21fe020d6018c6be2696939227a3a
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2020-03-19 22:28:33 -0600

    Agrupamiento: Rellenado de db

---
+++ aprendiendo-sql/05-agrupamiento/insert.sql
@@ -0,0 +1,17 @@
+-- Categorias
+insert into categorias values(null, 'Acción');
+insert into categorias values(null, 'Rol');
+insert into categorias values(null, 'Deportes');
+
+-- Entradas
+insert into entradas values(null, 1, 1, 'Novedades de GTA V Online', 'Review de GTA V', curdate());
+insert into entradas values(null, 1, 2, 'Review de LOL online', 'Todo sobre el lol', curdate());
+insert into entradas values(null, 1, 3, 'Nuevos jugadores de fifa 20', 'jugadores de fifa', curdate());
+
+insert into entradas values(null, 2, 1, 'Novedades de Assasings Online', 'Review de Assasings', curdate());
+insert into entradas values(null, 2, 2, 'Novedades de WOW Online', 'Review de WOW', curdate());
+insert into entradas values(null, 2, 3, 'Novedades de PES Online', 'Review de PES', curdate());
+
+insert into entradas values(null, 3, 1, 'Novedades de Call of Dutty Online', 'Review de Call of Dutty', curdate());
+insert into entradas values(null, 3, 1, 'Novedades de fornite Online', 'Review de fornite', curdate());
+insert into entradas values(null, 3, 3, 'Novedades de f1 Online', 'Review de f1', curdate());
