1ff63bb8ea5e56f41398115be68395d5a54391a1
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2020-04-01 21:30:27 -0600

    MVC: implementando MVC

---
+++ aprendiendo-php-mvc/models/Usuario.php
@@ -0,0 +1,14 @@
+<?php
+
+class Usuario {
+    public $nombre;
+    public $apellidos;
+    public $email;
+    public $password;
+    
+    
+    public function conseguirTodos() {
+        return "Sancando todos los usuarios";
+    }
+    
+}
---
+++ aprendiendo-php-mvc/views/usuarios/mostrar-todos.php
@@ -0,0 +1,4 @@
+<h1>
+    <?= $todos_los_usuarios ?>
+</h1>
+
