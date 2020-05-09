5952f1ee47149cfcdae3e55218ae6c594865e10f
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2020-05-08 20:59:33 -0500

    Imagenes de la aplicacion

--- laravel/proyecto-laravel/resources/views/home.blade.php
+++ laravel/proyecto-laravel/resources/views/home.blade.php
@@ -15,10 +15,12 @@
                             </div>
                         @endif
                         <div class="data-user">
-                            {{$image->user->name . ' ' . $image->user->surname }}
-                            <span class="nickname">
-                                {{' | @' . $image->user->nick}}
-                            </span>
+                            <a href="{{ route('image.detail', ['id' => $image->id]) }}">
+                                {{$image->user->name . ' ' . $image->user->surname }}
+                                <span class="nickname">
+                                    {{' | @' . $image->user->nick}}
+                                </span>
+                            </a>
                         </div>
                     </div>
 
@@ -26,14 +28,20 @@
                         <div class="image-container">
                             <img src="{{ route('image.file', ['filename' => $image->image_path]) }}" alt="">
                         </div>
-                        <div class="likes">
-
-                        </div>
                         <div class="description">
                             <span class="nickname"> {{'@' . $image->user->nick }} </span>
+                            <span class="nickname"> {{' | ' . \FormatTime::LongTimeFilter($image->created_at)}} </span>
                             <p>
                                 {{ $image->description }}
                             </p>
+                        </div>
+                        <div class="likes">
+                            <img src="{{ asset('img/gray-heart.png') }}" alt="unlike">
+                        </div>
+                        <div class="comments">
+                            <a href="" class="btn btn-sm btn-warning btn-comments">
+                                Comentarios ({{count($image->comments)}})
+                            </a>
                         </div>
                     </div>
                 </div>
---
+++ laravel/proyecto-laravel/resources/views/image/detail.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            @include('includes.message')
+            <div class="card pub_image">
+                <div class="card-header">
+                    @if($image->user->image)
+                        <div class="container-avatar">
+                            <img src=" {{ route('user.avatar', ['filename' => $image->user->image])}} " alt="Avatar de usuario" class="avatar">
+                        </div>
+                    @endif
+                    <div class="data-user">
+                        {{$image->user->name . ' ' . $image->user->surname }}
+                        <span class="nickname">
+                            {{' | @' . $image->user->nick}}
+                        </span>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <div class="image-container">
+                        <img src="{{ route('image.file', ['filename' => $image->image_path]) }}" alt="">
+                    </div>
+                    <div class="description">
+                        <span class="nickname"> {{'@' . $image->user->nick }} </span>
+                        <span class="nickname"> {{' | ' . \FormatTime::LongTimeFilter($image->created_at)}} </span>
+
+                        <p>
+                            {{ $image->description }}
+                        </p>
+                    </div>
+                    <div class="likes">
+                        <img src="{{ asset('img/gray-heart.png') }}" alt="unlike">
+                    </div>
+                    <div class="comments">
+                        <a href="" class="btn btn-sm btn-warning btn-comments">
+                            Comentarios ({{count($image->comments)}})
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
