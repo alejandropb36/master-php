9ca5eb090563ecb4cc52c9a66a5a31df8bd09d27
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2020-05-17 21:21:36 -0500

    Perfiles de usuarios

--- laravel/proyecto-laravel/resources/views/user/profile.blade.php
+++ laravel/proyecto-laravel/resources/views/user/profile.blade.php
@@ -4,7 +4,21 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @include('includes.message')
+            <div class="data-user row">
+                @if($user->image)
+                    <div class="container-avatar">
+                        <img src=" {{ route('user.avatar', ['filename' => $user->image])}} " alt="Avatar de usuario" class="avatar">
+                    </div>
+                @endif
+                <br>
+                <div class="user-info">
+                    <h1> {{'@' . $user->nick}} </h1>
+                    <h2> {{$user->name . ' ' . $user->surname }}</h2>
+                    <span class="nickname"> {{'Se unio: ' . \FormatTime::LongTimeFilter($user->created_at)}} </span>
+                </div>
+            </div>
+
+            <hr>
             
             @foreach ($user->images as $image)
                 @include('includes.image', ['image' => $image])
