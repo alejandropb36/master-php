49ad185a2cc0dd4a42c0f0b0bd1658a27c085b42
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2020-06-06 23:18:15 -0500

    Proyecto symfony: login

--- proyecto-symfony/src/Controller/UserController.php
+++ proyecto-symfony/src/Controller/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
 use App\Form\RegisterType;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
 {
@@ -46,6 +47,20 @@ class UserController extends AbstractController
 
         return $this->render('user/register.html.twig', [
             'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', [
+            'error' => $error,
+            'lastUsername' => $lastUsername
         ]);
     }
 }
